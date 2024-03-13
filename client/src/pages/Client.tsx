d566b22367d45205f2e9beedb346ce9b3391495d
Caleb Logan <clogan202@gmail.com>
2024-03-12 20:18:36 -0700

    adds workspace dropdown menu

--- client/src/pages/Client.tsx
+++ client/src/pages/Client.tsx
@@ -1,7 +1,9 @@
 import Header from "@/components/Header/Header";
 import "./Client.css";
-import { Home, MessagesSquare, Bell, Bookmark, MoreHorizontal } from "lucide-react";
+import { Home, MessagesSquare, Bell, Bookmark, MoreHorizontal, Plus } from "lucide-react";
 import { NavLink, Outlet } from "react-router-dom";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 export default function Client() {
     return (
@@ -19,6 +21,7 @@ export default function Client() {
 function Rail() {
     return <div className="rail">
         <div className="flex flex-col">
+            <WorkspaceLink name="B" />
             <RailLink to="" icon={<Home size={20} className="block m-auto" />} name="Home" end />
             <RailLink to="dms" icon={<MessagesSquare size={20} className="block m-auto" />} name="DMs" />
             <RailLink to="activity" icon={<Bell size={20} className="block m-auto" />} name="Activity" />
@@ -46,4 +49,28 @@ function RailLink({ to, icon, name, end }: { to: string, icon: React.JSX.Element
             )}
         </NavLink>
     </div>
+}
+
+function WorkspaceLink({ name }: { name: string }) {
+    return <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <button className={`text-xl mb-3 bg-gray-500 hover:bg-opacity-35 rounded-md w-[36px] h-[36px]`}>
+                {name}
+            </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="ml-2 min-w-[300px]">
+            {/* <Button className="flex flex-col pt-6 pb-6 items-start w-full">
+                <h3>Boogl</h3>
+                <p>juggle-hq.slack.com</p>
+            </Button> */}
+            <DropdownMenuItem className="flex flex-col items-start cursor-pointer">
+                <h3>Boogl</h3>
+                <p className="text-gray-600">juggle-hq.slack.com</p>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="text-md text-gray-500 cursor-pointer">
+                <Plus size={24} className="mr-4" /> Add a workspace
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
 }
